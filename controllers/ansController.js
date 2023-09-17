ef5d52ce47c413d0c7596087d0a72940465e5a79
Thawat <thawat@dtc.co.th>
2023-09-17 22:30:11 +0700

    แก้ไขให้แสดงชื่อข้อสอบ

--- controllers/ansController.js
+++ controllers/ansController.js
@@ -1,8 +1,16 @@
+const { data } = require("jquery");
+
 exports.handleLogout = (req, res) => {
     res.clearCookie('loggedIn');
     res.redirect('/login');
 };
 
 exports.render_ansPage = function (req, res) {
-    res.render('ans', { title: 'ans' })
+    let results = {
+        year: req.query.year,
+        term: req.query.term,
+        topic: req.query.topic,
+        sub_term: req.query.sub_term
+    }
+    res.render('ans', { title: 'ans', data: results })
 }
--- controllers/exam_manageController.js
+++ controllers/exam_manageController.js
@@ -11,10 +11,14 @@ exports.render_manageExamPage = function (req, res) {
 
 exports.insertData = async function (req, res) {
     try {
-        const { year, term, topic, sub_term } = req.body;
+        const { year, term, topic, sub_term, submit } = req.body;
         const result = await userModel.postAddANS(year, term, topic, sub_term);
 
-        res.send('<script>alert("บันทึกสำเร็จ"); window.location.href = "/exammanage";</script>');
+        if (submit == 'submit2') {
+            res.send(`<script>alert("บันทึกสำเร็จ"); window.location.href = "/ans?year=${year}&term=${term}&topic=${topic}&sub_term=${sub_term}";</script>`);
+        } else {
+            res.send('<script>alert("บันทึกสำเร็จ"); window.location.href = "/exammanage";</script>');
+        }
     } catch (error) {
         res.send('<script>alert("ไม่สามารถบันทึกได้"); window.location.href = "/exammanage";</script>');
     }
